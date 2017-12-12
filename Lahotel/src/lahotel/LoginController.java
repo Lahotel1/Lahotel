f70f2a19351ffcfc43b9551df8c5f1dd31a0fbdf
mormixz <Tuchchapol.m@gmail.com>
2017-12-13 00:35:37 +0700

    link page1

--- Lahotel/src/lahotel/LoginController.java
+++ Lahotel/src/lahotel/LoginController.java
@@ -5,13 +5,17 @@
  */
 package lahotel;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -21,6 +25,10 @@ public class LoginController implements Initializable {
 
     @FXML
     private TextField inputuser;
+
+    
+    @FXML
+    private AnchorPane backpane;
 
     @FXML
     private PasswordField inputpassword;
@@ -33,12 +41,18 @@ public class LoginController implements Initializable {
 
     @FXML
     void Login(ActionEvent event) {
-
+            
     }
 
     @FXML
-    void Register(ActionEvent event) {
-
+    void Register(ActionEvent event) throws IOException {
+          FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Register.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        RegisterController controller = fxmlLoader.<RegisterController>getController();
+        fxmlLoader.setController(controller);
+        backpane.getChildren().setAll(root);
     }
-
+    public void success(){
+        
+    }
 }
--- Lahotel/src/lahotel/RegisterController.java
+++ Lahotel/src/lahotel/RegisterController.java
@@ -5,12 +5,16 @@
  */
 package lahotel;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -29,7 +33,10 @@ public class RegisterController implements Initializable {
 
     @FXML
     private TextField email;
-
+    
+     @FXML
+    private AnchorPane backpane;
+     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
@@ -37,12 +44,24 @@ public class RegisterController implements Initializable {
     }
 
     @FXML
-    void back(ActionEvent event) {
-
+    void back(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        LoginController controller = fxmlLoader.<LoginController>getController();
+        fxmlLoader.setController(controller);
+        backpane.getChildren().setAll(root);
     }
 
     @FXML
-    void register(ActionEvent event) {
-
+    void register(ActionEvent event) throws IOException {
+        
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+         LoginController controller = fxmlLoader.<LoginController>getController();
+        fxmlLoader.setController(controller);
+        backpane.getChildren().setAll(root);
+    }
+    public void success(){
+        
     }
 }
