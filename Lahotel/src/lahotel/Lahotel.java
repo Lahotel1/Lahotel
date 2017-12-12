7387e119723c0fe69ef672cf90a2882e0b8848b2
Sivakorn <sivakornterk@gmail.com>
2017-12-13 02:42:12 +0700

    2ed2d

--- Lahotel/src/lahotel/Lahotel.java
+++ Lahotel/src/lahotel/Lahotel.java
@@ -29,7 +29,7 @@ public class Lahotel extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("LOGIN");
-        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.setScene(new Scene(root, 1280 , 720));
         primaryStage.show();
     }
 
